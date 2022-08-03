ebed6bc6b01ab87ecedba889e4790c02c05f7101
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-08-03 02:01:25 -0800

    Add files via upload

---
+++ WriteContent.java
@@ -0,0 +1,21 @@
+package write.content;
+import java.io.FileOutputStream;
+
+public class WriteContent
+{
+    public static void main(String[] args)
+    {
+        String txt = "This is the content to write into file using FileOutputStream";
+        try
+        {
+            FileOutputStream output = new FileOutputStream("C:\\Glassfish\\demo1.txt");
+            byte[] array = txt.getBytes();
+            output.write(array);
+            output.close();
+        }
+        catch (Exception e)
+        {
+            e.getStackTrace();
+        }
+    }   
+}
